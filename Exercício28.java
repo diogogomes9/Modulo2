2027d4fa5e74eac06d3f8d447d4234ab463fb9a3
diogogomes9 <32169058+diogogomes9@users.noreply.github.com>
2017-10-26 17:27:58 +0100

    Add files via upload

---
+++ Exercício28.java
@@ -0,0 +1,16 @@
+package Cálculos;
+import java.io.*;
+public class Exercício28 {
+
+	public static void main(String[] args) throws IOException {
+	
+	String C= "";	
+	System.out.print("Digite um digito:");
+	InputStreamReader LerS = new InputStreamReader (System.in);
+	BufferedReader GuardaS = new BufferedReader (LerS);
+	C = GuardaS.readLine();
+	System.out.printf("Digitou:%d", Integer.valueOf(C));
+
+	}
+
+}
