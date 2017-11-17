8e5f26e23134b918e4c22450e8a5ab1adb4ca230
diogogomes9 <32169058+diogogomes9@users.noreply.github.com>
2017-11-17 16:27:29 +0000

    Add files via upload

---
+++ Maximo3.java
@@ -0,0 +1,30 @@
+package maximo3;
+
+import java.util.Scanner;
+
+public class Maximo3 {
+
+    
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        double q, w , e;
+        double max = 0;
+        System.out.print("Qual o primeiro numero?");
+        q =ler.nextDouble();
+        System.out.print("Qual o segundo numero?");
+        w = ler.nextDouble();
+        System.out.print("Qual o terceiro numero?");
+        e = ler.nextDouble();
+        if (q>w && q>e)
+        max = q;
+        
+        if (w>q && w>e)
+        max = w;
+        if (e>q && e>w)
+        max = e;
+        
+        System.out.print("O máximo é" +max);
+        
+    }
+    
+}
