19db09dac17e86dc5a11be4f4310efa8673e43a1
diogogomes9 <32169058+diogogomes9@users.noreply.github.com>
2017-11-24 14:56:21 +0000

    Add files via upload

---
+++ HojeÉSexta.java
@@ -0,0 +1,12 @@
+package hojeésexta;
+
+public class HojeÉSexta {
+
+ 
+    public static void main(String[] args) {
+        String mensagem= "Hoje é Sexta-feira!";
+        for (int I=1; I<=5; I++)
+        System.out.println (mensagem);
+    }
+    
+}
